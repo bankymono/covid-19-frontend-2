860becdd91fb89ec1a2486bed863b7f9d573883f
Bankole Ayodeji <bankymono@gmail.com>
2020-04-14 12:57:50 +0100

    Updates

---
+++ precache-manifest.2b6181cb85f9f4dcabdc9bbcbc576ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef093467b3556396f2096a67f114c88",
+    "url": "/covid-19-frontend-2/index.html"
+  },
+  {
+    "revision": "c0341a876ff49abf4bf5",
+    "url": "/covid-19-frontend-2/static/css/main.87cf7b67.chunk.css"
+  },
+  {
+    "revision": "6f155cd0b5002fffb28b",
+    "url": "/covid-19-frontend-2/static/js/2.83dd2b31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-frontend-2/static/js/2.83dd2b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0341a876ff49abf4bf5",
+    "url": "/covid-19-frontend-2/static/js/main.27fa5f24.chunk.js"
+  },
+  {
+    "revision": "842e31124118b05e0372",
+    "url": "/covid-19-frontend-2/static/js/runtime-main.77ea6419.js"
+  }
+]);
